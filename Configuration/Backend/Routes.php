26f82517ff29a59174dac74924f13284152de49a
SvenJuergens <SvenJuergens@users.noreply.github.com>
2023-11-01 18:11:38 +0100

    [!!!][TASK] remove cacheAction for removing processed Files ... has the potential to kill the server if you delete a few thousand files at once and then have them regenerated all at once

--- Configuration/Backend/Routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'tx_cswebp' => [
-        'path' => '/cs_webp/clear',
-        'target' => \SvenJuergens\CsWebp\Controller\ClearImagesController::class . '::clearImages'
-    ]
-];
